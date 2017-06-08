46631a25dce5c3eaa06c17e89bf0094c3d79a67e
Sanghyeon Lee <isseebx38@gmail.com>
2017-06-08 23:21:31 +0900

    fixed untracked file, BlackjackApplication psvm추가

--- src/main/java/com/hw/team8/blackjack/BlackjackApplication.java
+++ src/main/java/com/hw/team8/blackjack/BlackjackApplication.java
@@ -4,4 +4,8 @@ package com.hw.team8.blackjack;
  * Created by com on 2017-06-08.
  */
 public class BlackjackApplication {
+    public static void main(String[] args) {
+        Game game = new Game();
+        game.start();
+    }
 }
