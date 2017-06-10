94c8ce71fd3d4e38351d073642e7ed8759b4666d
Ji Hun <tjrwlgns1198@gmail.com>
2017-06-10 14:23:13 +0900

    Player Hand상속 해제 후 코드 수정

--- src/main/java/com/hw/team8/blackjack/CardEvaluator.java
+++ src/main/java/com/hw/team8/blackjack/CardEvaluator.java
@@ -4,4 +4,29 @@ package com.hw.team8.blackjack;
  * Created by com on 2017-06-08.
  */
 public class CardEvaluator {
+
+    public int getBlackjackRank(Player player){
+        int val = 0;
+        boolean ace = false;
+        PlayerHand  playerHand = player.getPlayerHand();
+        int cards = playerHand.getCardCount();
+
+        for( int i=0; i< cards; i++){
+            Card card = playerHand.getCard(i);
+            int cardVal = card.getRank();
+            if(cardVal > 10){
+                cardVal = 10;
+            }
+            if(cardVal == 1){
+                ace = true;
+            }
+            val = val + cardVal;
+        }
+
+        if(ace == true && val + 10 <= 21)
+            val = val+10;
+
+        return val;
+    }
+
 }
--- src/test/java/com/hw/team8/blackjack/PlayerTest.java
+++ src/test/java/com/hw/team8/blackjack/PlayerTest.java
@@ -12,23 +12,25 @@ public class PlayerTest {
 
     @Test
     public void Player가_가지고있는_카드의_합은19다(){
-        Player playerHand = new Player();
+        Player player = new Player(new PlayerHand());
         Card card1 = new Card(10,Suit.SPADES);
         Card card2 = new Card(9,Suit.HEARTS);
+        CardEvaluator cardEvaluator = new CardEvaluator();
 
-        playerHand.addCard(card2);
-        playerHand.addCard(card1);
-        assertThat(playerHand.getBlackjackRank(), is(19));
+        player.getPlayerHand().addCard(card2);
+        player.getPlayerHand().addCard(card1);
+        assertThat(cardEvaluator.getBlackjackRank(player), is(19));
     }
 
     @Test
     public void Player가_가지고있는_카드의_합은21다(){
-        Player playerHand = new Player();
+        Player player = new Player(new PlayerHand());
         Card card1 = new Card(1,Suit.SPADES);
         Card card2 = new Card(10,Suit.HEARTS);
+        CardEvaluator cardEvaluator = new CardEvaluator();
 
-        playerHand.addCard(card2);
-        playerHand.addCard(card1);
-        assertThat(playerHand.getBlackjackRank(), is(21));
+        player.getPlayerHand().addCard(card2);
+        player.getPlayerHand().addCard(card1);
+        assertThat(cardEvaluator.getBlackjackRank(player), is(21));
     }
 ;}
