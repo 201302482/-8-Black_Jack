4808577df2b1339dbfff4ffcfcfb55aededc7e40
Lim Dae Yeong <limdae07@gmail.com>
2017-06-10 13:38:57 +0900

    GameTest test문(시작 금액은 100달러이다.)

--- src/test/java/com/hw/team8/blackjack/GameTest.java
+++ src/test/java/com/hw/team8/blackjack/GameTest.java
@@ -1,7 +1,19 @@
 package com.hw.team8.blackjack;
 
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 /**
- * Created by com on 2017-06-08.
+ * Created by com on 2017-06-08.s
  */
 public class GameTest {
+
+    @Test
+    public void 시작_금액은_100달러이다() {
+        Game game = new Game();
+        assertThat(game.getMoney(), is(100));
+    }
+
 }
